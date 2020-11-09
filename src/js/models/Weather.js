e4fa412f15b9006aeadbe333c8033bbcc11552c8
KirianaBrown <kjb0369@arastudent.ac.nz>
2020-11-09 18:03:42 +1300

    store individual results in the this of the obj

--- src/js/models/Weather.js
+++ src/js/models/Weather.js
@@ -21,6 +21,8 @@
 //     }
 // }
 
+import axios from 'axios';
+
 export default class Weather {
     constructor(query) {
         this.query = query;
@@ -30,6 +32,16 @@ export default class Weather {
         const API_KEY = `c8d2013b39ce1062ff9e54042c8d2be6`;
         const res = await axios(`https://api.openweathermap.org/data/2.5/weather?q=${this.query}&units=metric&appid=${API_KEY}`);
         this.results = res.data;
+        this.id = this.results.id;
+        this.name = this.results.name;
+        this.country = this.results.sys.country;
+        this.temp = this.results.main.temp;
+        this.tempMax = this.results.main.temp_max;
+        this.tempMin = this.results.main.temp_min;
+        this.description = this.results.weather[0].description;
+        // this.id = results.id;
+
+        console.log(this.id, this.name, this.country, this.temp, this.tempMax, this.tempMin, this.description)
 
         console.log(this.results)
     }
