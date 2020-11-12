aae6ba1d1f92324d04eea4b42c14d3c5bb60eea6
KirianaBrown <kjb0369@arastudent.ac.nz>
2020-11-12 19:19:00 +1300

    round main temp and include metric as parameter for func

--- src/js/views/weatherView.js
+++ src/js/views/weatherView.js
@@ -1,19 +1,20 @@
 import { elements } from './base';
 
-export const renderWeather = weather => {
+export const renderWeather = (weather, metric = 'C') => {
     const html =
         `
     <div class="results__location heading-tertiary">${weather.name} <sup class='country'> ${weather.sys.country} </sup></div>
      
       <div class="details">
         <div class="details__temperature">
-            ${weather.main.temp} C
+            ${Math.round(weather.main.temp)}${metric}
         </div>
 
         <div class="details__description">
             <img src="./img/weather/${weather.weather[0].icon}.svg" alt="Weather Icon"/>
             <br>
             <p class="details__description-secondary">${weather.weather[0].main}</p>
+            <p class="details__description-secondary">The high today will be ${weather.main.temp_max}${metric} currently feels like ${weather.main.feels_like}${metric}</p>
         </div>
       </div> 
   
