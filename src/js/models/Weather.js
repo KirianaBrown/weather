3f479e57f896111d3a17bc6c3ae20fc626815abd
KirianaBrown <kjb0369@arastudent.ac.nz>
2020-11-16 11:05:50 +1300

    remove irrelevant code

--- src/js/models/Weather.js
+++ src/js/models/Weather.js
@@ -1,26 +1,3 @@
-// export default class Weather {
-//     constructor(weatherObj) {
-//         this.weatherObj = weatherObj;
-//         this.id = weatherObj.id;
-//         this.city = weatherObj.name;
-//         this.country = weatherObj.sys.country;
-//         this.temp = weatherObj.main.temp;
-//     }
-
-//     setMetric() {
-//         this.type = 'deg'
-//     }
-
-//     showWeatherTemp() {
-//         console.log(this.weatherObj);
-//         console.log(this.id);
-//         console.log(this.city);
-//         console.log(this.country);
-//         console.log(this.temp);
-//         console.log(this.type);
-//     }
-// }
-
 import axios from 'axios';
 
 export default class Weather {
@@ -34,15 +11,6 @@ export default class Weather {
         this.results = res.data;
         this.metric = 'C';
         this.id = this.results.id;
-        // this.name = this.results.name;
-        // this.country = this.results.sys.country;
-        // this.temp = this.results.main.temp;
-        // this.tempMax = this.results.main.temp_max;
-        // this.tempMin = this.results.main.temp_min;
-        // this.description = this.results.weather[0].description;
-        // this.id = results.id;
-
-        // console.log(this.id, this.name, this.country, this.temp, this.tempMax, this.tempMin, this.description)
 
         console.log(this.results)
     }
