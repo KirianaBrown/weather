c9414085bea81f09192f9bd6ca1269fcd8bb81e6
KirianaBrown <kjb0369@arastudent.ac.nz>
2020-11-16 18:54:21 +1300

    create clearError function

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -3,3 +3,4 @@ import { elements } from './base';
 export const getInput = () => elements.searchInput.value;
 export const clearInput = () => elements.searchInput.value = '';
 export const clearUI = () => elements.weatherDetails.innerHTML = '';
+export const clearError = () => elements.errorMessage.innerHTML = '';
