c5ef2bf4f5295df764e486975ddeb82f29a7947f
KirianaBrown <kjb0369@arastudent.ac.nz>
2020-11-19 09:41:02 +1300

    consolidate unit model

--- src/js/models/Units.js
+++
@@ -1,7 +0,0 @@
-export const convertToFarenheit = (celsius) => {
-    return Math.floor(celsius * 9 / 5 + 32)
-}
-
-export const convertToCelsius = (farenheit) => {
-    return Math.floor((farenheit - 32) * 5 / 9);
-}
