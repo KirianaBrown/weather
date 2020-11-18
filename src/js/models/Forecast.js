638a9c92b60237f245eb0a775e5eff0f57ea1a89
KirianaBrown <kjb0369@arastudent.ac.nz>
2020-11-18 17:28:01 +1300

    clean

--- src/js/models/Forecast.js
+++ src/js/models/Forecast.js
@@ -9,7 +9,6 @@ export default class Forecast {
         const API_KEY = `${process.env.API_KEY}`;
         const res = await axios(`https://api.openweathermap.org/data/2.5/forecast?q=${this.query}&appid=${API_KEY}&units=metric`)
         this.forecast = res.data;
-        console.log(this.forecast);
         // 6am 9am 12pm 3pm 6pm 9pm 12pm 3am 6am 
         this.forecastArr = [
             this.forecast.list[8],
@@ -18,12 +17,10 @@ export default class Forecast {
             this.forecast.list[32],
             this.forecast.list[39],
         ];
-        console.log(this.forecastArr);
 
         this.forecastWeather = this.forecastArr.map(el => ({
             temp: Math.round(el.main.temp),
             dateTime: el.dt,
-            temp_min: Math.round(el.main.temp_min),
             icon: el.weather[0].icon,
         }));
     }
