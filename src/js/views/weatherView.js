a31ffc50b6768b55224363172d76310bdd91e5e4
KirianaBrown <kjb0369@arastudent.ac.nz>
2020-11-18 17:33:23 +1300

    use state to set variables for unit and symbol

--- src/js/views/weatherView.js
+++ src/js/views/weatherView.js
@@ -1,6 +1,6 @@
 import { elements } from './base';
 
-export const renderWeather = (weather, isSaved, metric = 'C') => {
+export const renderWeather = (weather, symbol, isSaved) => {
     const html =
         `
     <div class="results__location heading-tertiary">${weather.name} <sup class='country'> ${weather.sys.country} </sup>
@@ -16,14 +16,14 @@ export const renderWeather = (weather, isSaved, metric = 'C') => {
      
       <div class="details">
         <div class="details__temperature">
-            ${Math.round(weather.main.temp)} <span class='details__temperature-metric'> &#0176;${metric} </span>
+            ${Math.round(weather.main.temp)} <span class='details__temperature-metric'> &#0176;${symbol} </span>
         </div>
 
         <div class="details__description">
             <img class="details__fig" src="./img/weather/${weather.weather[0].icon}.svg" alt="Weather Icon"/>
             <br>
             <p class="details__description-secondary">${weather.weather[0].main}</p>
-            <p class="details__description-secondary">The high today will be ${Math.round(weather.main.temp_max)}&#0176;${metric} currently feels like ${Math.round(weather.main.feels_like)}&#0176;${metric}</p>
+            <p class="details__description-secondary">The high today will be ${Math.round(weather.main.temp_max)}&#0176;${symbol} currently feels like ${Math.round(weather.main.feels_like)}&#0176;${symbol}</p>
         </div>      
     </div>
 
