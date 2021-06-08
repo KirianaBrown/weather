46ff33ad53d4e323870196d428e2e4cd3ee76f66
KirianaBrown <kjb0369@arastudent.ac.nz>
2021-06-08 13:25:06 +1200

    test link

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,13 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default class Search {
     constructor(query) {
-        this.query = query
+        this.query = query;
     }
 
     async getResults() {
         const API_KEY = `${process.env.API_KEY}`;
-        const res = await axios(`https://api.openweathermap.org/data/2.5/weather?q=${this.query}&units=metric&appid=${API_KEY}`);
+
+        if (API_KEY) {
+            console.log("We have an API KEY");
+        } else {
+            console.log("NO API KEY LOADED");
+        }
+
+        console.log(API_KEY);
+        const res = await axios(
+            `https://api.openweathermap.org/data/2.5/weather?q=${this.query}&units=metric&appid=${API_KEY}`
+        );
         this.results = res.data;
         // console.log(this.results)
     }
