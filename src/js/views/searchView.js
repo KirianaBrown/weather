1365ddba23ac25b41f161cdd22fa2cde43cd7884
KirianaBrown <kjb0369@arastudent.ac.nz>
2022-04-28 15:03:09 +1200

    TEST bug fix in prod

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -9,27 +9,28 @@ export const clearError = () => (elements.errorMessage.innerHTML = "");
 export const clearForecast = () => (elements.forecastList.innerHTML = "");
 
 export const clearUI = () => {
-    console.log("clear ui is called");
-    // const cntnt = elements.weatherDetails;
+    const cntnt = elements.weatherDetails;
 
-    // if (process.env.MY_ENV === "development") {
-    //     if (cntnt.childNodes.length === 5) {
-    //         // Default setting
-    //         return;
-    //     }
+    if (process.env.MY_ENV === "development") {
+        console.log("development");
+        if (cntnt.childNodes.length === 5) {
+            // Default setting
+            return;
+        }
 
-    //     while (cntnt.childNodes.length > 5) {
-    //         // Remove all but the main containers
-    //         cntnt.removeChild(cntnt.lastChild);
-    //     }
-    // } else {
-    //     const container = document.querySelector(".container-results");
+        while (cntnt.childNodes.length > 5) {
+            // Remove all but the main containers
+            cntnt.removeChild(cntnt.lastChild);
+        }
+    } else {
+        console.log("production");
+        const container = document.querySelector(".container-results");
 
-    //     if (container.childNodes === 1) {
-    //         return;
-    //     }
-    //     while (container.childNodes.length > 3) {
-    //         container.removeChild(container.lastChild);
-    //     }
-    // }
+        if (container.childNodes === 1) {
+            return;
+        }
+        while (container.childNodes.length > 3) {
+            container.removeChild(container.lastChild);
+        }
+    }
 };
